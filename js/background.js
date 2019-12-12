13588fed5f70ef3bde0f17b3aa15ace914169ae1
Shahed Nasser <shahednasser@gmail.com>
2019-12-12 11:17:15 +0000

    add listener for extension installed and tab removed events

--- js/background.js
+++ js/background.js
@@ -1,12 +1,7 @@
 const searchTabs = {};
 chrome.webRequest.onCompleted.addListener(function (details) {
     const urlObj = new URL(details.url)
-    if(urlObj.href.indexOf('google.com') !== -1 && urlObj.pathname.indexOf('/search') !== -1) {
-        const searchQuery = urlObj.searchParams.get('q');
-        if(searchQuery) {
-            searchTabs[details.tabId] = searchQuery;
-        }
-    }
+    checkForSearchQuery(details.tabId, details.url)
 }, {urls: ["<all_urls>"]}, []);
 
 chrome.runtime.onMessage.addListener(function (request, sender, callback) {
@@ -15,3 +10,28 @@ chrome.runtime.onMessage.addListener(function (request, sender, callback) {
     });
     return true;  // Will respond asynchronously.
 });
+
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    if(searchTabs.hasOwnProperty(tabId)) {
+        delete searchTabs[tabId];
+    }
+});
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    console.log("installed");
+    chrome.tabs.query({}, function(tabs) {
+        for(let i = 0; i < tabs.length; i++) {
+            checkForSearchQuery(tabs[i].tabId, tabs[i].url);
+        }
+    });
+});
+
+function checkForSearchQuery(tabId, url) {
+    const urlObj = new URL(url)
+    if(urlObj.href.indexOf('google.com') !== -1 && urlObj.pathname.indexOf('/search') !== -1) {
+        const searchQuery = urlObj.searchParams.get('q');
+        if(searchQuery) {
+            searchTabs[tabId] = searchQuery;
+        }
+    }
+}
