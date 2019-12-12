3e8bfef0742d4637f2e5c0dac2d917467b543e7c
Shahed Nasser <shahednasser@gmail.com>
2019-12-12 10:38:05 +0000

    remove clear storage

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,4 @@
-chrome.storage.sync.clear();
+//chrome.storage.sync.clear();
 chrome.runtime.sendMessage({}, function (searchQuery) {
     if(searchQuery) {
         $("input[name=search_query]").val(searchQuery);
