a1cd1a265eadb9130634017fee727cad12ddf30f
Shahed Nasser <shahednasser@gmail.com>
2019-12-12 09:04:24 +0000

    added save functionality

--- js/popup.js
+++ js/popup.js
@@ -8,3 +8,21 @@ chrome.runtime.sendMessage({}, function (response) {
         $("input[name=search_result]").val(tabs[0].url);
     });
 });
+
+$("#saveSearchResult").on('click', function () {
+    const searchQuery = $("input[name=search_query]").val(),
+          searchResult = $("input[name=search_result]").val();
+    const self = this;
+    self.prop('disabled', true);
+    chrome.storage.sync.get(['search_results'], function (result) {
+        const search_results = result.search_results;
+        if(!search_results.hasOwnProperty(searchQuery)) {
+            search_results[searchQuery] = [];
+        }
+        search_results[searchQuery].push(searchResult);
+        chrome.storage.sync.set({search_results}, function () {
+            console.log("success");
+            self.prop('disabled', false);
+        });
+    });
+});
