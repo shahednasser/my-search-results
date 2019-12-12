a7918338c4e4650199c1c25e6cc4932203b1d8c7
Shahed Nasser <shahednasser@gmail.com>
2019-12-12 08:55:28 +0000

    fixes

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ const searchTabs = {};
 chrome.webRequest.onCompleted.addListener(function (details) {
     console.log("here1", details.url);
     const urlObj = new URL(details.url)
-    if(urlObj.href.indexOf('google.com') !== -1 && urlObj.pathname.indexOf('/search?') !== -1) {
+    if(urlObj.href.indexOf('google.com') !== -1 && urlObj.pathname.indexOf('/search') !== -1) {
         console.log("here2");
         const searchQuery = urlObj.searchParams.get('q');
         if(searchQuery) {
