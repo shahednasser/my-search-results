f290cf1209ed35d99dc084556e435989b1244fdd
Shahed Nasser <shahednasser@gmail.com>
2019-12-12 10:14:47 +0000

    add link to save search result

--- js/allResults.js
+++ js/allResults.js
@@ -7,11 +7,14 @@ const resultsList = $("#results");
 $("#searchQuery").text(searchQuery);
 chrome.storage.sync.get(['search_results'], function(result) {
     if(!result.search_results || !result.search_results.hasOwnProperty(searchQuery)) {
-        window.location.href = chrome.runtime.getURL('popup.html')
+        window.location.href = chrome.runtime.getURL('popup.html');
     }
 
     const results = result.search_results[searchQuery];
     for(let i = 0; i < results.length; i++) {
         resultsList.append("<li>" + results[i] + '</li>');
     }
+});
+$("#saveSearchResult").on('click', function () {
+    window.location.href = chrome.runtime.getURL('popup.html');
 });
