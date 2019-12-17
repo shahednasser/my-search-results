fba6682d0bcbaf47f75e01856df16bb075361a36
Shahed Nasser <shahednasser@gmail.com>
2019-12-17 08:41:12 +0000

    check if search query exists or similar to existing one

--- js/background.js
+++ js/background.js
@@ -29,8 +29,45 @@ function checkForSearchQuery(tabId, url) {
     const urlObj = new URL(url)
     if(urlObj.href.indexOf('google.com') !== -1 && urlObj.pathname.indexOf('/search') !== -1) {
         const searchQuery = urlObj.searchParams.get('q');
+        chrome.pageAction.show(tabId);
         if(searchQuery) {
+            //check if search query is similar to saved search queries
+            chrome.storage.sync.get(['search_results'], function(result) {
+                if(result.search_results) {
+                    const similarQuery = checkSimilarQueries(result.search_results, searchQuery);
+                    if(similarQuery) {
+                        chrome.browserAction.setBadgeText({text: result.search_results[searchQuery].length, tabId});
+                        chrome.pageAction.setTitle({tabId, text: 'You have similar saved search results!'});
+                    } else {
+                        chrome.browserAction.setBadgeText({text: '', tabId});
+                        chrome.pageAction.setTitle({tabId, text: ''});
+                    }
+                } else {
+                    chrome.browserAction.setBadgeText({text: '', tabId});
+                    chrome.pageAction.setTitle({tabId, text: ''});
+                }
+            });
             searchTabs[tabId] = searchQuery;
         }
+    } else {
+        chrome.pageAction.hide(tabId);
     }
+}
+
+function checkSimilarQueries(search_results, searchQuery) {
+    if(search_results.hasOwnProperty(searchQuery)) {
+        return searchQuery;
+    }
+
+    let arr = searchQuery.split(" ");
+    const keys = Object.keys(search_results);
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < keys.length; j++) {
+            if(keys[j].indexOf(arr[i]) !== -1) {
+                return keys[j];
+            }
+        }
+    }
+
+    return null;
 }
