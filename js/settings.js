95c65893f32a3fabc75c4c88cea804d5a41a801e
Shahed Nasser <shahednasser@gmail.com>
2019-12-19 12:17:38 +0000

    fix for getting element

--- js/settings.js
+++ js/settings.js
@@ -1,21 +1,18 @@
 const showBadgeElement = $("#show_badge"),
-      getNotificationsElement = $("#getNotifications");
+      getNotificationsElement = $("#get_notifications");
 chrome.storage.sync.get(['settings'], function(results) {
     let show_badge = true,
         get_notifications = false;
-        console.log(results.settings);
     if(results.settings) {
         show_badge = results.settings.hasOwnProperty('show_badge') ? results.settings.show_badge : true;
         get_notifications = results.settings.hasOwnProperty('get_notifications') ? results.settings.get_notifications : false;
     }
-    console.log(show_badge, get_notifications);
     showBadgeElement.prop('checked', show_badge);
     getNotificationsElement.prop('checked', get_notifications);
 });
 
 $("#saveBtn").on('click', function () {
     let settings = {show_badge: showBadgeElement.is(":checked"), get_notifications: getNotificationsElement.is(":checked")};
-    console.log(settings);
     const self = this;
     $(self).prop('disabled', true);
     chrome.storage.sync.set({settings}, function () {
